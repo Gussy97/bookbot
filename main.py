0b882f4b12fc7bc764636ad2dc16709e73839b64
Gussy97 <angus.scheiwe1997@hotmail.com>
2025-02-23 16:31:11 +1000

    Updating to match with updated course

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import sys
+
 def main():
-    path_to_book = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_book = sys.argv[1]
     create_report(path_to_book)
 
 def count_words(text):
